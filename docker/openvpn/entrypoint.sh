0386bd6c2eac01f9effcd1eb19128c44c378b09e
xrelkd <46590321+xrelkd@users.noreply.github.com>
2020-10-17 03:11:14 +0800

    feat: update `docker-openvpn` image

--- docker/openvpn/entrypoint.sh
+++ docker/openvpn/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "Starting OpenVPN client..."
+echo "Starting OpenVPN client as a daemon"
 openvpn --config /config.ovpn \
   --auth-nocache \
   --daemon
 
-echo "Starting Tunelo..."
+echo "Starting Tunelo $(tunelo version)"
 exec tunelo socks-server --ip 0.0.0.0 --port 8118
