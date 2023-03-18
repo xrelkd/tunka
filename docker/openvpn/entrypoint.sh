4a81242341c6a3d73f50e68912211dd41695e0bd
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-03-19 04:37:55 +0800

    style: format

--- docker/openvpn/entrypoint.sh
+++ docker/openvpn/entrypoint.sh
@@ -5,14 +5,14 @@ AUTH_FILE="/auth.txt"
 
 echo "Starting OpenVPN client as a daemon"
 if [ -e $AUTH_FILE ]; then
-  openvpn --config $CONFIG_FILE \
-    --auth-user-pass $AUTH_FILE \
-    --auth-nocache \
-    --daemon
+    openvpn --config $CONFIG_FILE \
+        --auth-user-pass $AUTH_FILE \
+        --auth-nocache \
+        --daemon
 else
-  openvpn --config $CONFIG_FILE \
-    --auth-nocache \
-    --daemon
+    openvpn --config $CONFIG_FILE \
+        --auth-nocache \
+        --daemon
 fi
 
 echo "Starting Tunelo $(tunelo version)"
